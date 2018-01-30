81a267e9520bea0eb3ecae2c13d3ba92235d4a5e
vlad-vs <vlad-vs@yandex.ru>
2018-01-30 12:12:09 +0200

    firstCommit

---
+++ test.js
@@ -0,0 +1,52 @@
+
+
+// var str = "oto";
+// var str2 = "";
+
+// for (var i = 0; i < str.length; ++i) {
+// 	if(str[i] !== " "){
+// 		str2 = str2 + str[i];
+// 	} 
+// }
+
+// document.write(str2);
+
+// /////////////////////////////////////////////////
+
+// var count = str.length / 2;
+
+// var b = true;
+
+// var counter = 0;
+
+// for (var j = 0; j < str.length; ++j) {
+//  	if (str[j] === str[str.length - j- 1]) {
+//  		counter = counter + 1;
+//  		if (counter === count ) {
+//  			break;
+//  		}
+//  	} 
+//  	else b = false;
+//  } 
+
+//  if (b) {
+//  	 alert('Yes');
+//  } else alert('no');
+
+ // //////////////////////////////////////////////
+
+var inVar = document.getElementById('strIn');
+alert(inVar);
+
+var randomVar = (Math.random() * 10).toFixed
+
+function ifndNumber(chose, numberToFind) {
+	if (numberToFind === chose) {
+		alert('yes');
+	}
+else alert('no')
+}
+
+for(var i = 0; i < 2; ++i){
+	ifndNumber(2,randomVar);
+}

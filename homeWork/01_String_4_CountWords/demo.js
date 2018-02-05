ecb600e4d8121ca285693c73b671cf48995dc1e7
vlad-vs <vlad-vs@yandex.ru>
2018-02-05 13:08:59 +0200

    add new tasks to readme.md

--- homeWork/01_String_4_CountWords/demo.js
+++
@@ -1,8 +0,0 @@
-// Количество слов в строке
-// Вводится строка, состоящая из слов, разделенных пробелами. Требуется посчитать количество слов в ней.
-//+
-
-var strFromUser = 'hello world'.trim();
-var arrWords = strFromUser.split(' ');
-alert(arrWords.length);
-

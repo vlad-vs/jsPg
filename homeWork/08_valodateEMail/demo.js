ae038ee1329bf2ae24df66eaede63c6076aa4ae8
vlad-vs <vlad-vs@yandex.ru>
2018-02-21 18:06:17 +0200

    add home work 4

--- homeWork/08_valodateEMail/demo.js
+++ homeWork/08_valodateEMail/demo.js
@@ -1,9 +1,26 @@
-
-let strFromUser = 'vlad-vs@yandex.ru';
-
-let r = /(^[^.][0-9a-zA-Z! #$%&'*+\-/=?^_`{|}~.]{1,64})(@)([^-0-9][-0-9a-zA-Z]{1,63})(.)([a-z]{1,})/i;
+let str = 'vlad-vs@yandex.ru';
 
 
-if (r.test(strFromUser)){
-   document.body.innerHTML = 'Валидация пройдена';
-} else document.body.innerHTML = 'Ошибка, валидация не пройдена!!!!';
+function validateEmail(strFromUser) {
+    let reg1 = /^([^.][0-9a-zA-Z! #$%&'*+\-/=?^_`{|}~.]{1,64}[^.$])@([^-0-9][-0-9a-zA-Z]{1,63})\.([a-z]{2,3})$/i;
+    let reg2 = /\.{2,}/i;
+
+    // основная проверка (кроме 2 точки подряд)
+    if (reg1.exec(strFromUser) === null) {
+        document.body.innerHTML = 'Ошибка, валидация не пройдена!!!!';
+    // проверка на 2 точки подряд
+    } else if (reg2.exec(a[0])) {
+        document.body.innerHTML = 'Ошибка, валидация не пройдена!!!!';
+    } else {
+        document.body.innerHTML = 'Валидация пройдена'
+    }
+}
+
+function validator(regularExp, str) {
+    return regularExp.exec(str) !== null;
+}
+
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
